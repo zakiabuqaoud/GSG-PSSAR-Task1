854d3e26ec5af546f4c7d2c2bc3872d8592bbd1b
zakiabuqaoud <20172106@student.alazhar.edu.ps>
2026-05-16 14:43:23 -0800

    create and read and print parquet file

--- main.py
+++ main.py
@@ -14,3 +14,27 @@ print("///" * 50)
 emp_data_frame_excel = pd.read_excel("raw_data/employee.xlsx")
 print("This is dataset in Excel File:")
 print(emp_data_frame_excel)
+
+# comma
+print("///" * 50)
+
+#dealing with parquet files
+
+# create my  data frame
+my_data = {
+    "names" : ["Zaki", "Omar", "Ahmed"],
+    "ages": [26,28,30]
+}
+my_data_frame = pd.DataFrame(my_data)
+
+# create parquet file
+my_data_frame.to_parquet("raw_data/employee.parquet")
+
+#read and print parquet file
+emp_data_frame_parquet = pd.read_parquet("raw_data/employee.parquet")
+print(emp_data_frame_parquet)
+
+
+
+
+
